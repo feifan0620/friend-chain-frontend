041a481421f72ab79bbe7f4a9688be62e6ad0445
feifan0620 <feifan0620@qq.com>
2024-09-14 16:45:57 +0800

    集成`vant ui`库以增强组件功能

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+// 1. 引入你需要的组件
+import { Button } from 'vant'
+// 2. 引入组件样式
+import 'vant/lib/index.css'
 
 const app = createApp(App)
 
+// 3. 注册你需要的组件
+app.use(Button)
 app.use(createPinia())
 app.use(router)
 
