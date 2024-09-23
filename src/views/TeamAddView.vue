c5ae7284cae5def43cc78d79b458a25fd40d7e05
feifan0620 <feifan0620@qq.com>
2024-09-24 02:15:14 +0800

    重构TeamAddView组件布局和功能
    - 重新排序Vue导入以优化组件加载。
    - 对formData对象进行调整，包括新增描述、过期时间和最大人数字段，以及状态字段的更改。
    - 重新配置表单项，移除未使用的级联选择器和步进器组件。
    - 更新表单验证规则以适应新的字段要求。
    -精简上传组件的使用，限制最大上传数量为1。
    - 修改创建按钮的文本为“创建”以提升用户体验。
    
    此次重构提升了TeamAddView的可用性和功能性，确保更好的用户交互和数据输入有效性。

--- src/views/TeamAddView.vue
+++ src/views/TeamAddView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { ref, reactive, defineProps, toRefs, onMounted } from 'vue'
-import { BrowseOffIcon } from 'tdesign-icons-vue-next'
+import { defineProps, onMounted, reactive, ref, toRefs } from 'vue'
 import { useRouter } from 'vue-router'
+
 const router = useRouter()
 
 const props = defineProps({
@@ -39,23 +39,16 @@ const files = ref([
     url: 'https://tdesign.gtimg.com/mobile/demos/example4.png',
     name: 'uploaded1.png',
     type: 'image'
-  },
-  {
-    url: 'https://tdesign.gtimg.com/mobile/demos/example6.png',
-    name: 'uploaded2.png',
-    type: 'image'
   }
 ])
 
 const formData = reactive({
   name: '',
+  description: '',
+  expireTime: '',
+  maxNum: 3,
+  status: '',
   password: '',
-  gender: '',
-  birth: '',
-  place: '',
-  age: 3,
-  description: 2,
-  resume: '',
   photo: files
 })
 const form = ref(null)
@@ -80,92 +73,14 @@ const onCancel = () => {
 
 const onConfirm = (value: string) => {
   console.log('confirm: ', value)
-  formData.birth = value
+  formData.expireTime = value
   visible.value = false
-}
-
-// 级联选择器
-const data = {
-  areaList: [
-    {
-      label: '北京市',
-      value: '110000',
-      children: [
-        {
-          value: '110100',
-          label: '北京市',
-          children: [
-            { value: '110101', label: '东城区' },
-            { value: '110102', label: '西城区' },
-            { value: '110105', label: '朝阳区' },
-            { value: '110106', label: '丰台区' },
-            { value: '110107', label: '石景山区' },
-            { value: '110108', label: '海淀区' },
-            { value: '110109', label: '门头沟区' },
-            { value: '110111', label: '房山区' },
-            { value: '110112', label: '通州区' },
-            { value: '110113', label: '顺义区' },
-            { value: '110114', label: '昌平区' },
-            { value: '110115', label: '大兴区' },
-            { value: '110116', label: '怀柔区' },
-            { value: '110117', label: '平谷区' },
-            { value: '110118', label: '密云区' },
-            { value: '110119', label: '延庆区' }
-          ]
-        }
-      ]
-    },
-    {
-      label: '天津市',
-      value: '120000',
-      children: [
-        {
-          value: '120100',
-          label: '天津市',
-          children: [
-            { value: '120101', label: '和平区' },
-            { value: '120102', label: '河东区' },
-            { value: '120103', label: '河西区' },
-            { value: '120104', label: '南开区' },
-            { value: '120105', label: '河北区' },
-            { value: '120106', label: '红桥区' },
-            { value: '120110', label: '东丽区' },
-            { value: '120111', label: '西青区' },
-            { value: '120112', label: '津南区' },
-            { value: '120113', label: '北辰区' },
-            { value: '120114', label: '武清区' },
-            { value: '120115', label: '宝坻区' },
-            { value: '120116', label: '滨海新区' },
-            { value: '120117', label: '宁河区' },
-            { value: '120118', label: '静海区' },
-            { value: '120119', label: '蓟州区' }
-          ]
-        }
-      ]
-    }
-  ]
-}
-const options = data.areaList
-
-const address = ref('120119')
-const visibleCascader = ref(false)
-
-const onChangeCascader = (value: string, options: any) => {
-  formData.place = options?.map((item: any) => item.label).join('/')
-  visibleCascader.value = false
-}
-
-const showCascader = () => {
-  visibleCascader.value = true
 }
 
 // 步进器
 const onChangeStepper = ($event: number) => {
-  formData.age = $event
+  formData.maxNum = $event
 }
-
-// rate
-const rateGap = 8
 
 // form
 const onReset = () => {
@@ -177,13 +92,11 @@ const onSubmit = (e: any) => {
 }
 
 const rules = {
-  name: [{ validator: (val: any) => val.length === 8, message: '只能输入8个字符英文' }],
-  password: [{ validator: (val: any) => val.length > 6, message: '长度大于6个字符' }],
-  gender: [{ validator: (val: any) => val !== '', message: '不能为空' }],
-  birth: [{ validator: (val: any) => val !== '', message: '不能为空' }],
+  name: [{ validator: (val: any) => val !== '', message: '不能为空' }],
+  status: [{ validator: (val: any) => val !== '', message: '不能为空' }],
+  expireTime: [{ validator: (val: any) => val !== '', message: '不能为空' }],
   place: [{ validator: (val: any) => val !== '', message: '不能为空' }],
-  description: [{ validator: (val: any) => val > 3, message: '分数过低会影响整体评价' }],
-  resume: [{ validator: (val: any) => val !== '', message: '不能为空' }]
+  description: [{ validator: (val: any) => val !== '', message: '不能为空' }]
 }
 
 onMounted(() => {
@@ -213,75 +126,11 @@ onMounted(() => {
       @reset="onReset"
       @submit="onSubmit"
     >
-      <t-form-item label="用户名" name="name" help="输入用户名">
-        <t-input v-model="formData.name" borderless placeholder="请输入内容"></t-input>
-      </t-form-item>
-      <t-form-item label="性别" name="gender">
-        <t-radio-group v-model="formData.gender" class="box" borderless @change="groupChangeFn">
-          <t-radio :block="false" name="radio" value="man" label="男" />
-          <t-radio :block="false" name="radio" value="women" label="女" />
-          <t-radio :block="false" name="radio" value="secret" label="保密" />
-        </t-radio-group>
-      </t-form-item>
-      <t-form-item arrow label="生日" name="birth" content-align="right">
-        <t-input
-          v-model="formData.birth"
-          borderless
-          align="right"
-          placeholder="请输入内容"
-          @click="visible = true"
-        ></t-input>
-        <t-popup v-model="visible" placement="bottom">
-          <t-date-time-picker
-            :value="formData.birth"
-            :mode="['date']"
-            title="选择日期"
-            start="2015-5-5"
-            format="YYYY-MM-DD"
-            @change="onChange"
-            @pick="onPick"
-            @confirm="onConfirm"
-            @cancel="onCancel"
-          />
-        </t-popup>
-      </t-form-item>
-      <t-form-item arrow label="籍贯" name="place" content-align="right">
-        <t-input
-          v-model="formData.place"
-          borderless
-          align="right"
-          placeholder="请输入内容"
-          @click="showCascader"
-        ></t-input>
-        <t-cascader
-          v-model:visible="visibleCascader"
-          :value="address"
-          title="选择地址"
-          :options="options"
-          @change="onChangeCascader"
-        />
-      </t-form-item>
-      <t-form-item label="年限" name="age" content-align="right">
-        <t-stepper v-model="formData.age" theme="filled" @change="onChangeStepper" />
-      </t-form-item>
-      <t-form-item label="自我评价" name="description" content-align="right">
-        <t-rate v-model="formData.description" variant="filled" allow-half :gap="rateGap" />
-      </t-form-item>
-      <t-form-item label="个人简介" name="resume">
-        <t-textarea
-          v-model="formData.resume"
-          class="textarea"
-          indicator
-          :maxlength="50"
-          placeholder="请输入个人简介"
-        ></t-textarea>
-      </t-form-item>
-      <t-form-item label="上传照片" name="photo">
+      <t-form-item label="队伍图片" name="photo">
         <t-upload
           class="upload"
           :default-files="formData.photo"
-          multiple
-          :max="6"
+          :max="1"
           :action="action"
           :on-fail="onFail"
           :on-progress="onProgress"
@@ -293,8 +142,52 @@ onMounted(() => {
         >
         </t-upload>
       </t-form-item>
+      <t-form-item label="队伍名称" name="name">
+        <t-input v-model="formData.name" borderless placeholder="请输入队伍名称"></t-input>
+      </t-form-item>
+      <t-form-item label="队伍描述" name="description">
+        <t-textarea
+          v-model="formData.description"
+          class="textarea"
+          indicator
+          :maxlength="50"
+          placeholder="请输入队伍描述"
+        ></t-textarea>
+      </t-form-item>
+      <t-form-item arrow label="过期时间" name="expireTime" content-align="right">
+        <t-input
+          v-model="formData.expireTime"
+          borderless
+          align="right"
+          placeholder="请选择时间"
+          @click="visible = true"
+        ></t-input>
+        <t-popup v-model="visible" placement="bottom">
+          <t-date-time-picker
+            :value="formData.expireTime"
+            :mode="['date']"
+            title="选择日期"
+            start="2024-1-1"
+            format="YYYY-MM-DD"
+            @change="onChange"
+            @pick="onPick"
+            @confirm="onConfirm"
+            @cancel="onCancel"
+          />
+        </t-popup>
+      </t-form-item>
+      <t-form-item label="最大人数" name="maxNum" content-align="right">
+        <t-stepper v-model="formData.maxNum" theme="filled" @change="onChangeStepper" />
+      </t-form-item>
+      <t-form-item label="队伍状态" name="status">
+        <t-radio-group v-model="formData.status" class="box" borderless @change="groupChangeFn">
+          <t-radio :block="false" name="radio" value="1" label="公开" />
+          <t-radio :block="false" name="radio" value="1" label="私有" />
+          <t-radio :block="false" name="radio" value="2" label="加密" />
+        </t-radio-group>
+      </t-form-item>
       <div class="button-group">
-        <t-button theme="primary" type="submit" size="large">提交</t-button>
+        <t-button theme="primary" type="submit" size="large">创建</t-button>
         <t-button theme="default" variant="base" type="reset" size="large">重置</t-button>
       </div>
     </t-form>
@@ -305,6 +198,9 @@ onMounted(() => {
 .team-add-navbar {
   --td-navbar-bg-color: #f5f7fc;
 }
+t-form-item[name='photo'] {
+}
+
 .box {
   width: 100%;
   display: flex;
